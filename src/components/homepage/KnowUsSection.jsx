20dcb3d317d212f214339135785a06c05d6a7430
Anurag Bansal <devanuragbansal@gmail.com>
2025-07-19 02:26:23 +0530

    Refactor KnowUsSection layout for improved semantics and image loading

--- src/components/homepage/KnowUsSection.jsx
+++ src/components/homepage/KnowUsSection.jsx
@@ -7,46 +7,55 @@ import mangoImage from "../../assets/mango.png";
 
 function KnowUsSection() {
   return (
-    <div className="w-full p-6 py-18 bg-[#94DA49] flex flex-col gap-12 items-center justify-center overflow-hidden">
+    <section className="w-full p-6 py-18 bg-[#94DA49] flex flex-col gap-12 items-center justify-center overflow-hidden">
       <div
         className="relative w-full min-h-[550px] max-w-6xl mx-auto flex flex-col items-center justify-center gap-y-12 bg-[#B2F56F] p-8 rounded-lg shadow-lg"
         style={{
           boxShadow: "10px 10px 0 20px #ffffff",
         }}
       >
+        {/* People Images */}
         <img
           src={abhishekLeftImage}
           alt="Abhishek"
-          className="absolute left-0 bottom-0 w-100 h-auto drop-shadow-xl z-2"
+          loading="lazy"
+          className="hidden md:block absolute left-0 bottom-0 w-100 drop-shadow-xl z-10"
         />
         <img
           src={nischayRightImage}
           alt="Nischay"
-          className="absolute right-0 bottom-0 w-100 h-auto drop-shadow-xl z-2"
+          loading="lazy"
+          className="hidden md:block absolute right-0 bottom-0 w-100 drop-shadow-xl z-10"
         />
 
+        {/* Fruits */}
         <img
           src={strawberryImage}
           alt="Strawberry"
-          className="absolute left-0 bottom-0 transform -translate-x-1/2 w-60 h-auto drop-shadow-xl shake-animation"
+          loading="lazy"
+          className="absolute left-0 bottom-0 -translate-x-1/2 w-40 md:w-60 drop-shadow-xl shake-animation z-20"
         />
         <img
           src={mangoImage}
           alt="Mango"
-          className="absolute right-0 bottom-0 transform translate-x-1/2 w-50 h-auto drop-shadow-xl shake-animation"
+          loading="lazy"
+          className="absolute right-0 bottom-0 translate-x-1/2 w-32 md:w-50 drop-shadow-xl shake-animation z-20"
         />
 
+        {/* Main Image */}
         <img
           src={knowUsImage}
           alt="Know Us"
-          className="w-80 h-auto relative z-10"
+          className="w-60 md:w-80 h-auto relative z-30"
         />
-        <p className="text-center text-4xl leading-normal font-semibold max-w-[360px] text-[#1A1A1A] relative z-10">
+
+        {/* Text */}
+        <p className="text-center text-2xl md:text-4xl leading-normal font-semibold max-w-[360px] text-[#1A1A1A] relative z-30">
           Fokus isn’t just a drink—it’s a lifestyle you live every day. Dive
           into our story and see what makes us different.
         </p>
       </div>
-    </div>
+    </section>
   );
 }
 
