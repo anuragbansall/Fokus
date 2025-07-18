95aadbf3ce0a43362b8293df61edec1bd1dc189a
Anurag Bansal <devanuragbansal@gmail.com>
2025-07-19 02:48:18 +0530

    Refactor ReviewCard component to use semantic HTML elements for improved accessibility

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ReviewCard({ name, image, reviewText }) {
   return (
-    <div
+    <figure
       className="bg-[#ffffea] rounded-lg overflow-hidden w-[22rem] border border-[#FFE060]"
       style={{
         boxShadow: "5px 5px 0 10px #ffffff",
@@ -12,17 +12,19 @@ function ReviewCard({ name, image, reviewText }) {
         <img
           src={image}
           alt={name}
+          title={`Review by ${name}`}
+          loading="lazy"
           className="w-full h-full object-cover object-top"
         />
       </div>
 
-      <div className="p-6">
+      <figcaption className="p-6">
         <h3 className="text-2xl text-center font-semibold mb-4">{name}</h3>
         <p className="text-lg text-gray-600 text-center mt-4 leading-relaxed">
           {reviewText}
         </p>
-      </div>
-    </div>
+      </figcaption>
+    </figure>
   );
 }
 
